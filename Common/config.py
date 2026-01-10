d3101ae540dc041eca6590249bafc69f632704ff
Navnit Kumar <60180825+navnit75@users.noreply.github.com>
2026-01-10 11:42:33 -0500

    Added comment on the maxMerkleTreeNode

--- Common/config.py
+++ Common/config.py
@@ -1,6 +1,12 @@
 hashSize = 256
 arity = 4
+
+# NOTE : This is author kept number for the leaf nodes when mekle tree size is calculated.
+# I am guessing its kept mostly to keep the parameters of experiment similar.
+# Hence I am adding an option to change this to `None` in which case it considers the original leaf node
+# i.e. nothing but original number of transactions a block has.
 maxMerkleTreeLeafNode = 15
+
 nodeCount = 5
 walletCountPerNode = 5
 incentiveAmount = 1
